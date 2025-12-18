73a1c97625baaecc8a44c03a0a3a1c2cf0b5ea64
niwamo <nm@niwamo.com>
2025-12-18 12:13:11 -0600

    bump wazuh revision (212-->217)

--- deployments/wazuh/ps6/main.tf
+++ deployments/wazuh/ps6/main.tf
@@ -47,7 +47,7 @@ module "wazuh" {
   wazuh_server = {
     app_name = "wazuh-server"
     channel  = "4.11/edge"
-    revision = 212
+    revision = 217
     config = {
       logs-ca-cert             = var.logs_ca_certificate
       custom-config-ssh-key    = "secret:${juju_secret.git_ssh_key.secret_id}"
