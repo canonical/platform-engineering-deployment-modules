23280227234b3673241e6e4b645cdf17b068cd0e
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2026-01-22 17:36:29 +0100

    Upgrade Wazuh server to rev 229 (#44)

--- deployments/wazuh/ps6/main.tf
+++ deployments/wazuh/ps6/main.tf
@@ -51,7 +51,7 @@ module "wazuh" {
   wazuh_server = {
     app_name = "wazuh-server"
     channel  = "4.11/edge"
-    revision = 225
+    revision = 229
     config = {
       logs-ca-cert             = var.logs_ca_certificate
       custom-config-ssh-key    = "secret:${juju_secret.git_ssh_key.secret_id}"
