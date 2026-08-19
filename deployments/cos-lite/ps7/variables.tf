697bbeca8b22e327d0ed8a3eeb77d5767489c94d
Sébastien Georget <sebastien.georget@canonical.com>
2026-08-19 19:01:39 +0200

    Remove unecessary module (#141)

--- deployments/cos-lite/ps7/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "model_uuid" {
-  description = "UUID of the Juju model where the application will be deployed"
-  type        = string
-}
-
-variable "external_hostname" {
-  description = "Hostname to configure on the ingress configurator charm"
-  type        = string
-}
