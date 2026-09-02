bbfd1ba5d89b18828f4ada99c2d8a845f26c8184
minulo <minh.nguyen@canonical.com>
2026-09-02 15:48:07 -0400

    fixing revision of ingress

--- deployments/gopkg/ps7/main.tf
+++ deployments/gopkg/ps7/main.tf
@@ -10,7 +10,7 @@ module "ingress_configurator" {
   model_uuid = var.model_uuid
   channel    = "latest/edge"
   # renovate: charm="ingress-configurator" track="latest" risk="edge" base="24.04" arch="amd64"
-  revision = 101
+  revision = 103
   config   = { hostname = var.external_hostname }
   trust    = true
 }
